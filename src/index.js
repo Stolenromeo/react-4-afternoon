8b0f34b240c12c31865726f2f05432a56898d1da
Tiler Smith <TilerSmith@MacBook-Air.local>
2018-07-24 16:18:05 -0600

    completed includes black diamond

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
 import App from './App';
+import {HashRouter as Route} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Route>
+    <App />
+</Route>
+, document.getElementById('root'));
